82f9bf3264cdb69c216c973e005484026727fd59
idpokute <mike.sayhi@gmail.com>
2021-08-12 05:42:43 -0400

    Code Spliting using State

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const onClick = async () => {
-    // const notify = await import("./notify");
-    // try {
-    //   notify.default();
-    //   notify.consoleTest();
-    // } catch (e) {
-    //   console.log(e);
-    // }
-
-    import("./notify").then((result) => {
-      result.default();
-      result.consoleTest();
-    });
+class App extends Component {
+  state = {
+    SplitMe: null,
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  onClick = async () => {
+    const loadedModule = await import("./components/SplitMe");
+    this.setState({ SplitMe: loadedModule.default });
+  };
 
-        <button onClick={onClick}>Greeting!</button>
-      </header>
-    </div>
-  );
+  render() {
+    const { SplitMe } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <button onClick={this.onClick}>Greeting!</button>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
