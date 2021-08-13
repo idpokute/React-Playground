32a79a632cffe05149b59d303704fc2baeea4afe
idpokute <mike.sayhi@gmail.com>
2021-08-13 06:21:43 -0400

    serve static files

--- src/index.server.js
+++ src/index.server.js
@@ -1,6 +1,65 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
+import path from "path";
+import fs from "fs";
 
-const html = ReactDOMServer.renderToString(<div>Hello SSR!</div>);
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8"),
+);
+const chunks = Object.keys(manifest.files)
+  .filter((key) => /chunk\.js$/.exec(key))
+  .map((key) => `<script src="${manifest.files[key]}"></script>`)
+  .join("");
 
-console.log(html);
+function createPage(root) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>React App</title>
+        <link href="${manifest.files["main.css"]}" rel="stylesheet" />
+    </head>
+    <body>
+        <noscript>You need to enable js to run this app</noscript>
+        <div id="root">
+            ${root}
+        </div>
+
+        <script src="${manifest.files["runtime-main.js"]}"></script>
+        ${chunks}
+        <script src="${manifest.files["main.js"]}"></script>
+    </body>
+    </html>
+    `;
+}
+
+const app = express();
+
+const serverRender = (req, res, next) => {
+  // 404가 뜨는 상황을 방지하고 서버사이드 렌더링을 해줌
+
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(createPage(root));
+};
+
+const serve = express.static(path.resolve("./build"), {
+  index: false,
+});
+app.use(serve);
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log("Running on http://localhost:5000");
+});
